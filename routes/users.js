6ec99374e81be77ba2e68cd8117caf839aed22a1
Lucas Lima <lucasrafael123araujolima@gmail.com>
2022-10-22 20:45:37 -0400

    feat: :bug: Conexão MySQL

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var coon = require("./../inc/db")
+var express = require("express")
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", function (req, res, next) {
+  coon.query("SELECT * FROM tb_users ORDER BY name", (err, results) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(results)
+    }
+  })
+})
 
-module.exports = router;
+module.exports = router
